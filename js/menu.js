ead579ae9090bce129e0213c40c031b7e0abc10b
PierreEv <piero12@free.fr>
2015-01-23 17:46:55 +0100

    Ajout page Admin (Modification + Historique) + Centralisation de la bar de menu de Admin

---
+++ js/menu.js
@@ -0,0 +1,2 @@
+$('#menu').replaceWith
+('<a href="accueil.html"><img src="../../images/logo_urjob.jpg" /></a>			<nav class="nav-link">				<ul>					<li><a href="accueil.html">Accueil</a></li>					<li><a href="support.html">Support</a></li>					<li><a href="faq.html">F.A.Q</a></li></ul></nav>');
