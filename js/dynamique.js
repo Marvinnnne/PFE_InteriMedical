21ba4829134c7ba59cdca8a6ff3839287ffc4ae1
Marvinnnne <marvinleemans@gmail.com>
2015-01-24 16:50:24 +0100

    Centralisation du menu déroulant et amélioration du css
    Signed-off-by: Marvinnnne <marvinleemans@gmail.com>

---
+++ js/dynamique.js
@@ -0,0 +1,3 @@
+$('#ho').replaceWith(
+	'<header class="header"> Urjob <ul class="niveau1">	<li class="survol"> Nom Prenom 	<ul class="niveau2"> <li> <a href="moncompte.html" id="MonCompte">Mon compte</a> </li> <li>	<a href="#" id="Deco">Déconnexion</a> </li> </ul> </li>	</ul> </header>'
+	);
