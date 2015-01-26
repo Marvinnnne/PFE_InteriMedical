74916aba08abf999061fabe81c1ec8c5f71c08b2
Marvinnnne <marvinleemans@gmail.com>
2015-01-26 18:03:37 +0100

    Page HTML cadre/accueil presque fini avec le css
    Signed-off-by: Marvinnnne <marvinleemans@gmail.com>

--- js/dynamique.js
+++ js/dynamique.js
@@ -1,3 +1,26 @@
-$('#ho').replaceWith(
-	'<header class="header"> Urjob <ul class="niveau1">	<li class="survol"> Nom Prenom 	<ul class="niveau2"> <li> <a href="moncompte.html" id="MonCompte">Mon compte</a> </li> <li>	<a href="#" id="Deco">Déconnexion</a> </li> </ul> </li>	</ul> </header>'
+$('#head').replaceWith(	'<header class="header"> 		<img id="logoUrjob" src="../../images/logo_urjob.jpg" />		<div class="col-sm-offset-8 col-sm-2">			<div class="input-group">      			<span class="input-group-btn">        			<button class="btn btn-default" type="button" aria-label="Left Align">        				<span class="glyphicon glyphicon-search"></span>        			</button>      			</span>      			<input type="text" class="form-control" placeholder="Search for...">    		</div>    	</div>    	  		<ul id="niveau1" class="nav navbar-nav">				<li class="survol"> Nom Prenom 					<b class="caret"></b>				<ul id="niveau2" class="dropdown-menu"> 					<li> <a href="moncompte.html" id="MonCompte" >Mon compte</a> </li>					<li class="divider"></li>					<li> <a href="#" id="Deco">Déconnexion</a> </li> 				</ul> 			</li>		</ul>	</header>'
 	);
+
+/*<header class="header"> 
+		<img id="logoUrjob" src="../../images/logo_urjob.jpg" />
+		<div class="col-sm-offset-8 col-sm-2">
+			<div class="input-group">
+      			<span class="input-group-btn">
+        			<button class="btn btn-default" type="button" aria-label="Left Align">
+        				<span class="glyphicon glyphicon-search"></span>
+        			</button>
+      			</span>
+      			<input type="text" class="form-control" placeholder="Search for...">
+    		</div>
+    	</div>    	
+  		<ul id="niveau1" class="nav navbar-nav">	
+			<li class="survol"> Nom Prenom 	
+				<b class="caret"></b>
+				<ul id="niveau2" class="dropdown-menu"> 
+					<li> <a href="moncompte.html" id="MonCompte" >Mon compte</a> </li>
+					<li class="divider"></li>
+					<li> <a href="#" id="Deco">Déconnexion</a> </li> 
+				</ul> 
+			</li>	
+		</ul> 	
+	</header>*/
