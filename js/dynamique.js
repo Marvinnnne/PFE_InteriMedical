35abba14eb529ebae6ae246bc399c159d3077e4d
Marvinnnne <marvinleemans@gmail.com>
2015-02-01 18:39:58 +0100

    Amélioration de un peu tout et ajout des icones
    Signed-off-by: Marvinnnne <marvinleemans@gmail.com>

--- js/dynamique.js
+++ js/dynamique.js
@@ -1,4 +1,4 @@
-$('#head').replaceWith(	'<header class="header"> 		<img id="logoUrjob" src="../../images/logo_urjob_only.png" />		<div class="col-sm-offset-8 col-sm-2">			<div class="input-group">      			<span class="input-group-btn">        			<button class="btn btn-default" type="button" aria-label="Left Align">        				<span class="glyphicon glyphicon-search"></span>        			</button>      			</span>      			<input type="text" class="form-control" placeholder="Search for...">    		</div>    	</div>    	  		<ul id="niveau1" class="nav navbar-nav">				<li class="survol"> Nom Prenom 					<b class="caret"></b>				<ul id="niveau2" class="dropdown-menu"> 					<li> <a href="moncompte.html" id="MonCompte" >Mon compte</a> </li>					<li class="divider"></li>					<li> <a href="#" id="Deco">Déconnexion</a> </li> 				</ul> 			</li>		</ul>	</header>'
+$('#head').replaceWith(	'<header class="header"> 	<a href="accueil.html">	<img id="logoUrjob" src="../../images/logo_urjob_only2_mini.png" />	</a>	<ul id="niveau1" class="nav navbar-nav">				<li class="survol"> Dupuis Jean 					<b class="caret"></b>				<ul id="niveau2" class="dropdown-menu"> 					<li> <a href="moncompte.html" id="MonCompte" >Mon compte</a> </li>					<li class="divider"></li>					<li> <a href="#" id="Deco">Déconnexion</a> </li> 				</ul> 			</li>		</ul>	</header>'
 	);
 
 /*<header class="header"> 
@@ -12,7 +12,11 @@ $('#head').replaceWith(	'<header class="header"> 		<img id="logoUrjob" src="../.
       			</span>
       			<input type="text" class="form-control" placeholder="Search for...">
     		</div>
+<<<<<<< HEAD
+    	</div>
+=======
     	</div>    	
+>>>>>>> fdb8616754c20efc5ee7e704d30b1ea43d7bdb64
   		<ul id="niveau1" class="nav navbar-nav">	
 			<li class="survol"> Nom Prenom 	
 				<b class="caret"></b>
