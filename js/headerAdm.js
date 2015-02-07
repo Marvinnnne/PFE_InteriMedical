08259864a1b1f380caf300a2fcdd1c0f38189d5e
Marvinnnne <marvinleemans@gmail.com>
2015-02-08 00:30:19 +0100

    Signed-off-by: Marvinnnne <marvinleemans@gmail.com>

--- js/headerAdm.js
+++ js/headerAdm.js
@@ -1,4 +1,4 @@
-$('#head').replaceWith(	'<header class="header"> 	<a href="accueil.html">	<img id="logoUrjob" src="../../images/logo_urjob_only2_mini.png" />	</a>	<ul id="niveau1" class="nav navbar-nav">				<li class="survol"> Dupuis Jean 					<b class="caret"></b>				<ul id="niveau2" class="dropdown-menu"> 					<li> <a href="moncompte.html" id="MonCompte" >Mon compte</a> </li>					<li class="divider"></li>					<li> <a href="../Urjob.html" id="Deco">Déconnexion</a> </li> 				</ul> 			</li>		</ul>	</header>'
+$('#headAdm').replaceWith(	'<header class="header"> 	<a href="accueil.html">	<img id="logoUrjob" src="../../images/logo_urjob_only2_mini.png" />	</a>	<ul id="niveau1" class="nav navbar-nav">				<li class="survol"> Kiroul Pierre 					<b class="caret"></b>				<ul id="niveau2" class="dropdown-menu"> 					<li> <a href="moncompte.html" id="MonCompte" >Mon compte</a> </li>					<li class="divider"></li>					<li> <a href="../Urjob.html" id="Deco">Déconnexion</a> </li> 				</ul> 			</li>		</ul>	</header>'
 	);
 
 /*<header class="header"> 
---
+++ js/headerInt.js
@@ -0,0 +1,17 @@
+$('#headInt').replaceWith(	'<header class="headerInt"> 	<a href="accueil.html">	<img id="logoUrjob" src="../../images/logo_urjob_only2_mini.png" />	</a>	<ul id="niveau1" class="nav navbar-nav">				<li class="survol"> Laje Carl 					<b class="caret"></b>				<ul id="niveau2" class="dropdown-menu"> 					<li> <a href="moncompte.html" id="MonCompte" >Mon compte</a> </li>					<li class="divider"></li>					<li> <a href="../Urjob.html" id="Deco">Déconnexion</a> </li> 				</ul> 			</li>		</ul>	</header>'
+	);
+
+/*<header class="headerInt">
+ 	<a href="accueil.html">	
+ 		<img id="logoUrjob" src="../../images/logo_urjob_only2_mini.png" />	
+ 	</a>
+ 	<ul id="niveau1" class="nav navbar-nav">
+ 		<li class="survol"> Dupuis Jean <b class="caret"></b>		
+ 			<ul id="niveau2" class="dropdown-menu">
+				<li> <a href="moncompte.html" id="MonCompte" >Mon compte</a> </li>
+				<li class="divider"></li>
+				<li> <a href="../Urjob.html" id="Deco">Déconnexion</a> </li>
+			</ul>
+		</li>
+	</ul>
+</header>*/
